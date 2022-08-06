4504fbbaec843a3c0f368e8f5472f4250d4fb16c
AvirukBasak <avirukbasak@gmail.com>
2022-08-07 01:37:34 +0530

    n/a: test

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.TG_TND_BOT_TOKEN, { polling: true });
+const request = require('request');
+const express = require('express');
+const app = express();
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.TG_TND_BOT_TOKEN;
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+require('dotenv').config();
 
-// Matches "/echo [whatever]"
-bot.onText(/\/start (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, "Hajime!");
-});
+bot.onText(/\/movie (.+)/, (msg, match) => {
+    let movie = match[1];
+    let chatId = msg.chat.id;
+    request(`http://www.omdbapi.com/?apiKey=${process.env.OMD_API_KEY}=${movie}`, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            bot.sendMessage(chatId, '_Looking for _' + movie + '...', { parse_mode: 'Markdown' })
+                .then((msg) => {
+                    let res = JSON.parse(body);
+                    bot.sendPhoto(chatId, res.Poster, { caption: 'Result: \nTitle: ' + res.Title + '\nYear: ' + res.Year + '\nRated: ' + res.Rated + '\nReleased: ' + res.Released + '\nRuntime: ' + res.Runtime + '\nGenre: ' + res.Genre + '\nDirector: ' + res.Director + '\nPlot: ' + res.Plot })
+                        .catch((err) => {
+                            if (err) {
+                                bot.sendMessage(chatId, 'Error in finding,Check the movie title');
+                            }
+                        })
+                })
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+        }
+    })
+})
+
+bot.onText(/\/about (.+)/, (msg, match) => {
+    if (match[1]) {
+        let chatId = msg.chat.id;
+        bot.sendMessage(chatId,
+            `finder bot\nbot where you can search for a movie and get details about it.`, { parse_mode: 'Markdown' });
+    }
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log(`Server is running at port ${PORT}`);
 });
