a83e3a35c60d568d27d2bfff973ed54d8cba64af
AvirukBasak <avirukbasak@gmail.com>
2022-08-07 09:22:03 +0530

    n/a: test

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.TG_TND_BOT_TOKEN, { polling: true });
-const request = require('request');
-const express = require('express');
-const app = express();
+const TOKEN = process.env.TG_TND_BOT_TOKEN;
+const bot = new TelegramBot(TOKEN, { polling: true });
 
+console.log("tok: " + TOKEN.toString().substring(TOKEN.length -6, TOKEN.length -1));
 
-require('dotenv').config();
+bot.on('message', (msg) => {
+    console.log(msg);
+    bot.sendMessage(msg.chat.id, "hi");
+});
 
-
-
-bot.onText(/\/movie (.+)/, (msg, match) => {
-    let movie = match[1];
+bot.onText(/\/about/, (msg, match) => {
     let chatId = msg.chat.id;
-    request(`http://www.omdbapi.com/?apiKey=${process.env.OMD_API_KEY}=${movie}`, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            bot.sendMessage(chatId, '_Looking for _' + movie + '...', { parse_mode: 'Markdown' })
-                .then((msg) => {
-                    let res = JSON.parse(body);
-                    bot.sendPhoto(chatId, res.Poster, { caption: 'Result: \nTitle: ' + res.Title + '\nYear: ' + res.Year + '\nRated: ' + res.Rated + '\nReleased: ' + res.Released + '\nRuntime: ' + res.Runtime + '\nGenre: ' + res.Genre + '\nDirector: ' + res.Director + '\nPlot: ' + res.Plot })
-                        .catch((err) => {
-                            if (err) {
-                                bot.sendMessage(chatId, 'Error in finding,Check the movie title');
-                            }
-                        })
-                })
-
-
-        }
-    })
-})
-
-bot.onText(/\/about (.+)/, (msg, match) => {
-    if (match[1]) {
-        let chatId = msg.chat.id;
-        bot.sendMessage(chatId,
-            `finder bot\nbot where you can search for a movie and get details about it.`, { parse_mode: 'Markdown' });
-    }
-})
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, function () {
-    console.log(`Server is running at port ${PORT}`);
+    bot.sendMessage(chatId,
+        `finder bot\nbot where you can search for a movie and get details about it.`, { parse_mode: 'Markdown' });
 });
