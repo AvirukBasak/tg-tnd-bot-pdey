795326130d5f0b6dbb2d7be7d01e2a8ae3058ce1
AvirukBasak <avirukbasak@gmail.com>
2022-08-07 10:06:15 +0530

    n/a: test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const token = process.env.AUTH_TOKEN;
 
 const bot = new TelegramBot(token, { polling:true });
 
-bot.on('message', (msg) => {
-    console.log(msg);
-    bot.sendMessage(msg.chat.id, "hello, I recieved msg from you");
+bot.onText('message', (msg) => {
+    // console.log(msg);
+    const codeblock = "```";
+    bot.sendMessage(msg.chat.id, `${codeblock}${JSON.stringify(msg, null, 2)}${codeblock}`);
 });
 
 bot.on("polling_error", (msg) => console.log(msg));
