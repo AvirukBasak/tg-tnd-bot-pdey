dd56b53c849d2f6c5d4863f0ae4ba4326239f112
AvirukBasak <avirukbasak@gmail.com>
2022-08-12 22:10:51 +0530

    n/a

--- src/index.js
+++ src/index.js
@@ -40,14 +40,14 @@ const DEV_CMD = (
 
 // info
 const NOTES = (
-    `**${BOT_NAME}**:\n\n`
+    `${BOT_NAME}:\n\n`
     + "Sources: https://github.com/AvirukBasak/tg-tnd-bot-pdey\n"
     + "License: MIT\n\n" +
-    "**Notes**:\n\n"
+    "Notes:\n\n"
     + "The above list doesn't contain four commands. Out of which, the `debug` command should be avoided.\n\n"
     + "The `debug` command logs replies from the bot and player usernames become visible to the server admin.\n\n"
     + "Data regarding games and debug states reset 4 times a day. Upside is, any unlogged data is cleared. Downside is, a running game may get abruptly stopped.\n\n" +
-    "**Data Collection**:\n\n"
+    "Data Collection:\n\n"
     + "Every time a valid command is run, a hash of chat ID and message ID along with the name of that command is logged. This data helps server admin understand how much the bot is used.\n\n"
     + "Data collected doesn't provide information about the users or groups.\n\n"
     + "For details, visit the link in `Sources` section in /notes.\n"
@@ -270,8 +270,7 @@ Bot.onText(/^\/notes/, (msg, match) => {
         return;
     Bot.sendMessage(
         msg.chat.id,
-        NOTES,
-        { parse_mode: "markdown" }
+        NOTES
     );
 });
 
